44358251bf191bde02f86f6c7ff17a6b9f5880a1
subaru45 <cerasus45@gmail.com>
2014-11-04 10:18:18 +0900

    Shows wavy animation for the present

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject stagbeetle "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject stagbeetle "0.1.0"
+  :description "Battle animations of MOTHER 2"
+  :url "http://github.com/subaru45/stagbeetle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [seesaw "1.4.4"]]
   :main ^:skip-aot stagbeetle.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/stagbeetle/core.clj
+++ src/stagbeetle/core.clj
@@ -1,7 +1,71 @@
 (ns stagbeetle.core
-  (:gen-class))
+  (:gen-class)
+  (:use [seesaw.core]
+        [seesaw.graphics]
+        [stagbeetle.graphics]))
+
+(set! *warn-on-reflection* true)
+
+(import (java.awt.image BufferedImage
+                        Raster
+                        WritableRaster)
+        (java.awt Component
+                  Graphics
+                  Graphics2D))
+
+
+(def +fps+ 30)
+(def +width+ 320)
+(def +height+ 240)
+
+(def frm (frame :title "stagbeetle"))
+(def cnv (canvas))
+(def ^BufferedImage img
+  (buffered-image +width+ +height+ BufferedImage/TYPE_INT_ARGB))
+
+(defn paint [^Component c ^Graphics g]
+  (try (.drawImage g img 0 0 nil)))
+
+
+;;; drawing thread
+(def +stop+ (ref true))
+(defn stop [] (dosync (ref-set +stop+ true)))
+(defmacro on-drawing-thread [& body]
+  `(do (dosync (alter +stop+ (constantly false)))
+       (.start (Thread.
+                #(while (false? @+stop+)
+                   ~@body
+                   (repaint! cnv)
+                   (Thread/sleep (/ 1000 +fps+)))))))
+
+(defn init []
+  (native!)
+  (config! cnv :size [+width+ :by +height+])
+  (config! cnv :paint paint)
+  (config! frm :content cnv))
+
+(defn show []
+  (-> frm pack! show!))
+
+(defn draw-wave []
+  (draw-stripe! (.getRaster img) 20 0 [255 255 255 255] [90 170 255 255])
+  (repaint! frm)
+  (Thread/sleep 1000)
+  (transform-transverse! (.getRaster img) (.getData img)
+                         #(Math/sin %) 20 10 0)
+  (repaint! frm)
+  (Thread/sleep 1000)
+  (let [^WritableRaster des (.getRaster img)
+        ^Raster src (.getData img)
+        ph (ref 0)]
+    (on-drawing-thread
+     (transform-longitudinal! des src #(Math/sin %) 3 5 @ph)
+     (dosync (alter ph #(mod (+ % 0.1) 6.28))))))
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (init)
+  (config! frm :on-close :exit)
+  (draw-wave)
+  (show))
